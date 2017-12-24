febdcaef43df9b22284ab12841e4167d740a326a
dimathe47 <dimathe47@yandex.ru>
2017-12-25 01:44:14 +0300

    draw selected with bigger pen width

--- selected_graphics_rect.py
+++ selected_graphics_rect.py
@@ -1,6 +1,7 @@
 import typing
 from PyQt5 import QtGui
 from PyQt5.QtCore import QRectF, QRect, Qt
+from PyQt5.QtGui import QPen, QColor
 from PyQt5.QtWidgets import QGraphicsItem, QWidget
 
 
@@ -15,4 +16,9 @@ class SelectedGraphicsRect(QGraphicsItem):
 
     def paint(self, painter: QtGui.QPainter, option: 'QStyleOptionGraphicsItem',
               widget: typing.Optional[QWidget] = ...):
+        painter.save()
+        pen=QPen(QColor(0,0,0,255))
+        pen.setWidth(5)
+        painter.setPen(pen)
         painter.drawRect(self.qrectf)
+        painter.restore()
