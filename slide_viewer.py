1f8c8399f8ecf2df9a3840009d815ff7e9536ebb
dimathe47 <dimathe47@yandex.ru>
2018-01-20 20:38:27 +0300

    remove logical_zoom and physical_zoom reolve current level by scene size and view scale

--- slide_viewer.py
+++ slide_viewer.py
@@ -67,27 +67,16 @@ class SlideViewer(QWidget):
         self.scene.clear()
         self.scene.addItem(self.slide_graphics)
 
-        max_level = self.slide_helper.get_max_level()
-        self.logical_zoom = 1 / self.slide_helper.get_downsample_for_level(max_level)
-
-        self.physical_zoom = 1
         self.current_level = self.slide_helper.get_max_level()
-
         self.fit_in_view()
-
         self.update_labels()
 
     def fit_in_view(self):
-        current_level = self.get_level_for_logical_zoom(self.logical_zoom)
-        self.slide_graphics.update_visible_level(current_level)
-        self.update_scene_rect_for_level(current_level)
+        self.slide_graphics.update_visible_level(self.current_level)
+        new_rect = self.slide_helper.get_rect_for_level(self.current_level)
+        self.scene.setSceneRect(new_rect)
         margins = QMarginsF(200, 200, 200, 200)
         self.view.fitInView(self.scene.sceneRect() + margins, Qt.KeepAspectRatio)
-        scale_ = self.view.transform().m11()
-        self.logical_zoom *= scale_
-
-        self.current_level = self.slide_helper.get_max_level()
-        self.physical_zoom *= scale_
 
     def eventFilter(self, qobj: 'QObject', event: 'QEvent'):
         self.eventSignal.emit(event)
@@ -128,7 +117,7 @@ class SlideViewer(QWidget):
     def remember_selected_rect_params(self):
         pos_scene = self.view.mapToScene(self.rubber_band.pos() - self.view.pos())
         rect_scene = self.view.mapToScene(self.rubber_band.rect()).boundingRect()
-        downsample = self.get_current_level_downsample()
+        downsample = self.slide_helper.get_downsample_for_level(self.current_level)
         selected_qrectf_0_level = QRectF(pos_scene * downsample,
                                          QSizeF(rect_scene.size() * downsample))
         self.selected_rect_0_level = selected_qrectf_0_level.getRect()
@@ -140,21 +129,23 @@ class SlideViewer(QWidget):
         self.update_scale(event.pos(), zoom_)
         event.accept()
 
-    def set_view_from_rect(self, rect_scene, visible_level):
-        # another way to adjust view - but scaling and translating will be not accurate
-        self.view.fitInView(rect_scene, Qt.KeepAspectRatio)
-        self.update_items_visibility_for_current_level()
+    # def set_view_from_rect(self, rect_scene, visible_level):
+    # another way to adjust view - but scaling and translating will be not accurate
+    # self.view.fitInView(rect_scene, Qt.KeepAspectRatio)
+    # self.update_items_visibility_for_current_level()
+    #
+    # def update_from_rect_and_downsample(self, rect, downsample):
+    #     self.update_scene_rect_for_current_level()
+    #     if rect:
+    #         self.view.translate(rect.x(), rect.y())
 
     def update_scale(self, mouse_pos: QPoint, zoom):
         old_mouse_pos_scene = self.view.mapToScene(mouse_pos)
         old_view_scene_rect = self.view.mapToScene(self.view.viewport().rect()).boundingRect()
 
-        # old_level_downsample = self.get_current_level_downsample()
-        # self.logical_zoom *= zoom
-        old_level = self.get_best_level_for_zoom(self.view.transform().m11())
+        old_level = self.get_best_level_for_scale(self.get_current_view_scale())
         old_level_downsample = self.slide_helper.get_downsample_for_level(old_level)
-        new_level = self.get_best_level_for_zoom(self.view.transform().m11() * zoom)
-        # new_level_downsample = self.get_current_level_downsample()
+        new_level = self.get_best_level_for_scale(self.get_current_view_scale() * zoom)
         new_level_downsample = self.slide_helper.get_downsample_for_level(new_level)
 
         level_scale_delta = 1 / (new_level_downsample / old_level_downsample)
@@ -165,52 +156,33 @@ class SlideViewer(QWidget):
         new_view_scene_rect = QRectF(new_view_scene_rect_top_left,
                                      old_view_scene_rect.size() * level_scale_delta / zoom)
 
-        # current_level = self.get_level_for_logical_zoom(self.logical_zoom)
-        # self.update_scene_rect_for_level(current_level)
-        self.update_scene_rect_for_level(new_level)
+        new_rect = self.slide_helper.get_rect_for_level(new_level)
+        self.scene.setSceneRect(new_rect)
 
-        scale_ = self.view.transform().m11() * zoom * new_level_downsample / old_level_downsample
-        transform = QTransform().scale(scale_, scale_).translate(-new_view_scene_rect.x(),
-                                                                 -new_view_scene_rect.y())
+        new_scale = self.get_current_view_scale() * zoom * new_level_downsample / old_level_downsample
+        transform = QTransform().scale(new_scale, new_scale).translate(-new_view_scene_rect.x(),
+                                                                       -new_view_scene_rect.y())
+        self.current_level = new_level
         self.reset_view_transform()
         self.view.setTransform(transform, False)
-        # self.update_items_visibility_for_current_level()
         self.slide_graphics.update_visible_level(new_level)
         self.update_labels()
-        # self.get_best_level_for_zoom()
 
-    def get_best_level_for_zoom(self, zoom):
+    def get_best_level_for_scale(self, scale):
         scene_width = self.scene.sceneRect().size().width()
         candidates = [0]
         for level in self.slide_helper.get_levels():
             w, h = self.slide.level_dimensions[level]
-            if scene_width * zoom  < w:
+            if scene_width * scale <= w:
                 candidates.append(level)
         best_level = max(candidates)
-        print(best_level)
         return best_level
 
-    def update_from_rect_and_downsample(self, rect, downsample):
-        self.update_scene_rect_for_level()
-        if rect:
-            self.view.translate(rect.x(), rect.y())
-
-    def update_scene_rect_for_level(self, level):
-        rect = self.slide_helper.get_rect_for_level(level)
-        # rect = QRectF(0, 0, 100000, 100000)
-        self.scene.setSceneRect(rect)
-
-    def update_items_visibility_for_current_level(self):
-        current_level = self.get_level_for_logical_zoom(self.logical_zoom)
-        self.slide_graphics.update_visible_level(current_level)
-
     def update_labels(self):
-        current_level = self.get_best_level_for_zoom(self.view.transform().m11())
-        # current_level = self.get_level_for_logical_zoom(self.logical_zoom)
-        level_downsample = self.slide.level_downsamples[current_level]
-        level_size = self.slide_helper.get_level_size_for_level(current_level)
+        level_downsample = self.slide.level_downsamples[self.current_level]
+        level_size = self.slide_helper.get_level_size_for_level(self.current_level)
         self.level_label.setText(
-            "current level, downsample, size: {}, {:.4f}, ({}, {})".format(current_level, level_downsample,
+            "current level, downsample, size: {}, {:.4f}, ({}, {})".format(self.current_level, level_downsample,
                                                                            *level_size))
         if self.selected_rect_0_level:
             self.selected_rect_label.setText(
@@ -227,15 +199,9 @@ class SlideViewer(QWidget):
         # print("dx after resetTransform:", self.view.transform().dx())
         # print("horizontalScrollBar after resetTransform:", self.view.horizontalScrollBar().value())
 
-    def get_level_for_logical_zoom(self, logical_zoom):
-        return self.current_level
-        return self.slide_helper.get_best_level_for_downsample(1 / logical_zoom)
-
-    def get_current_level_downsample(self):
-        return self.slide_helper.get_downsample_for_level(self.current_level)
-        current_level = self.get_level_for_logical_zoom(self.logical_zoom)
-        level_downsample = self.slide_helper.get_downsample_for_level(current_level)
-        return level_downsample
-
     def get_current_view_scene_rect(self):
         return self.view.mapToScene(self.view.rect()).boundingRect()
+
+    def get_current_view_scale(self):
+        scale = self.view.transform().m11()
+        return scale
